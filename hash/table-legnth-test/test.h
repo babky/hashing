0158cdfd99d6b62e2918af3195feb6973b19c31c
babky <babky@localhost>
2009-06-29 09:25:38 +0000

    * Vytvorenie testu.

---
+++ hash/table-legnth-test/test.h
@@ -0,0 +1,22 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "table.h"
+#include "function.h"
+#include "universal_function.h"
+#include "storage.h"
+#include "utils/equality_comparer.h"
+
+namespace Hash { namespace Tests {
+
+	class LengthTest {
+	public:
+		typedef Hash::Table<int, Hash::Utils::EqualityComparer<int>, Hash::UniversalFunctionCWLF, Hash::ChainedStorage> 
+			TableCWLF;
+
+		void runTest(void);
+	};
+
+} }
+
+#endif /* TEST_H */
