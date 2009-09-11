d704a1fddc62e76f51210e7427c07c0e46df154b
babky <babky@localhost>
2009-09-11 17:10:37 +0000

    * Test vsetkeho.

--- hash/table-test/table_test.h
+++ hash/table-test/table_test.h
@@ -6,26 +6,95 @@
 #include "universal_function.h"
 #include "storage.h"
 #include "utils/equality_comparer.h"
+#include "utils/storage_statistics.h"
 #include "default_test.h"
 
 namespace Hash { namespace Tests {
 
+	template<class HashTable>
 	class TableTest : public TestSuite::DefaultTest
 	{
 	public:
-		TableTest(void);
+		TableTest(void) {
+		}
 
-		virtual void runTest(void);
+		virtual void runTest(void) {
+			this->testStatistics();
+			this->simpleRehash();
+			this->testCopyCtor();
+			this->testMassiveFill();
+		}
 
 	private:
-		typedef Hash::Table<int, Hash::Utils::EqualityComparer<int>, Hash::UniversalFunctionCWLF, Hash::ChainedStorage> 
-			TableType;
+		template<class T, class Table>
+		class InsertOperation {
+		public:
+			InsertOperation(const T & argument, Table & table):
+			  table(&table),
+			  argument(argument)
+			{
+			}
 
-		virtual void fillData(TableType & t);
-		virtual void testStatistics(void);
-		virtual void simpleRehash(void);
-		virtual void testCopyCtor(void);
-		virtual void testMassiveFill(void);
+			void operator()(void) {
+				this->table->insert(this->argument);
+			}
+
+			Table * table;
+			T argument;
+		};
+
+		typedef HashTable TableType;
+
+		virtual void fillData(TableType & t) {
+			this->assertEqual(0, t.getSize(), "Empty table must have just 0 items.");
+			t.insert(3);
+			this->assertEqual(1, t.getSize(), "Table (after t.insert(3)) should contain only 1 element.");
+			this->assertException<std::exception>(InsertOperation<int, TableType>(3, t), "Inserting 3 causes exception.");
+			this->assertTrue(t.contains(3), "Table should contain item 3.");
+			this->assertFalse(t.contains(0), "Table should not contain item 0.");
+			this->assertTrue(t.remove(3), "Removal must be successful.");
+			this->assertFalse(t.contains(3), "Table should not contain item 3.");
+		}
+
+		virtual void testStatistics(void) {
+			TableType t;
+			this->fillData(t);
+			Hash::Utils::StorageStatistics stats = Hash::Utils::StorageStatistics();
+			t.computeStatistics(stats);
+			this->assertEqual(t.getLoadFactor(), stats.getLoadFactor(), "Load factors must be equal (1).");
+			t.insert(3);
+			this->assertEqual<size_t>(16, stats.getTableLength(), "Default table length is sufficient.");
+
+			stats.clear();
+			t.computeStatistics(stats);
+			this->assertEqual(t.getLoadFactor(), stats.getLoadFactor(), "Load factors must be equal (2).");
+		}
+
+		virtual void simpleRehash(void) {
+			TableType t;
+			t.rehash();
+			t.insert(3);
+			t.rehash();
+			t.insert(4);
+			t.rehash();
+		}
+
+		virtual void testCopyCtor(void) {
+			TableType t;
+			this->fillData(t);
+			TableType b;
+			b = t;
+		}
+
+		virtual void testMassiveFill(void) {
+			TableType t;
+			for (int i = 0; i < 50000; ++i) {
+				t.insert(i);
+			}
+
+			this->assertEqual(50000, t.getSize(), "After inserting n distinct elements we expect table length of n.");
+		}
+
 	};
 
 } }
--- hash/table-test/test.cpp
+++ hash/table-test/test.cpp
@@ -3,12 +3,17 @@
 
 int main(size_t, const char **) {
 	using namespace Hash;
+	using namespace Hash::Utils;
 	using namespace Hash::Tests;
 	using namespace std;
 
-	TableTest test;
-	test.runTest();
-	cout << test.getAssertionsResult();
+	TableTest<Table<int, EqualityComparer<int>, UniversalFunctionCWLF, ChainedStorage> > test_univ_cwlf;
+	test_univ_cwlf.runTest();
+	cout << test_univ_cwlf.getAssertionsResult();
+
+	TableTest<Table<int, EqualityComparer<int>, Function, ChainedStorage> > test_hash;
+	test_hash.runTest();
+	cout << test_hash.getAssertionsResult();
 
 	return 0;
 }
