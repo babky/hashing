fee270a44e84dd79b8fb54593b9f028be5b88583
babka <babka@localhost>
2009-10-30 16:16:13 +0000

    * Opravy prikazov a typografie.

--- text/command/compile.cmd
+++ text/command/compile.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/DP/command/compile.sh
-"C:\Program Files\Foxit Reader.exe" H:\Documents\DP\output\text.pdf
+"C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/docs/text/command/compile.sh
+"C:\Program Files\Foxit Reader.exe" H:\Documents\docs\text\output\text.pdf
