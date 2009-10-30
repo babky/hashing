481daed1678531d451309eb63c83f897a411f4ac
babka <babka@localhost>
2009-10-30 16:16:16 +0000

    * Viac vstupov.

--- test-linear/src/cwlf.py
+++ test-linear/src/cwlf.py
@@ -4,11 +4,11 @@ print 'N: '
 N = int(sys.stdin.readline())
 print 'm: '
 m = int(sys.stdin.readline())
-xVec = [1, 2, 3, 9, 15, 16]
+xVec = [1, 2, 3, 9, 15, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 43, 44]
 
-M = [(a, b) for b in range(N) for a in range(N)]
+M = [(a, b) for b in range(N) for a in range(1, N)]
 for x in xVec:
     M = [(a, b) for (a, b) in M if ((a * x + b) % N) % m == 0]
     print "For %d we have %d solutions" % (x, len(M))
-    # print "They're: ", M
+    #print "They're: ", M
     print
