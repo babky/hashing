982995c795752d754fd123d08cbf1a6615b39314
babka <babka@localhost>
2009-11-02 23:48:18 +0000

    + CWLF revised.

--- test-linear/src/cwlf/strong_universality.py
+++ test-linear/src/cwlf/strong_universality.py
@@ -6,9 +6,9 @@ print 'm: '
 m = int(sys.stdin.readline())
 xVec = [1, 2, 3, 9, 15, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 43, 44]
 
-M = [(a, b) for b in range(N) for a in range(1, N)]
+M = [(a, b) for b in range(N) for a in range(N)]
 for x in xVec:
-    M = [(a, b) for (a, b) in M if ((a * x + b) % N) % m == 0]
+    M = [(a, b) for (a, b) in M if ((a * x + b) % N) % m == 1]
     print "For %d we have %d solutions" % (x, len(M))
     #print "They're: ", M
     print
---
+++ test-linear/src/tests/cwlf/collision_functions/collision_functions_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from cwlf.collision_functions import CollisionFunctions
+
+class CollisionFunctionsTest(unittest.TestCase):
+
+    def test_collision_function_count(self):
+        cf = CollisionFunctions(17)
+        self.assertEqual(cf.compute_function((3, 15), 2), 4, "CWLF function computation test.")
+
+        cf.restrict_collision_functions(1)
+        self.assertEqual(17 ** 2, cf.get_colliding_function_count(), "CWLF function count.")
+
+        cf.restrict_collision_functions(2)
+        first_count = cf.get_colliding_function_count()
+        cf.restrict_collision_functions(2)
+        second_count = cf.get_colliding_function_count()
+        self.assertEqual(first_count, second_count, "CWLF function count - restriction.")
+
+if __name__ == '__main__':
+    unittest.main()
+
