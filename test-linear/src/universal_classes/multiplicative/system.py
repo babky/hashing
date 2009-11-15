75b2cb2c21b5206849b83da247af37f08ae5857e
babka <babka@localhost>
2009-11-15 16:56:32 +0000

    + System multiplikativny.

--- test-linear/src/universal_classes/multiplicative/system.py
+++ test-linear/src/universal_classes/multiplicative/system.py
@@ -1,29 +1,19 @@
-class MultiplicativeSystem:
+from universal_classes.system import UniversalSystem
+
+class MultiplicativeSystem(UniversalSystem):
     
     def __init__(self, p, m):
+        UniversalSystem.__init__(self)
+        
         self.functions = range(1, p);
         self.p = p
         self.m = m
-        self.colliding_elements = []
+        
+    def restrict_collision_functions(self, x):
+        UniversalSystem._restrict_collision_functions_list(self, x)
         
     def compute_function(self, function, x):
         return ((function * x) % self.p) % self.m 
         
-    def restrict_collision_functions(self, x):
-        if self.colliding_elements == []:
-            self.colliding_elements = [x]
-            return
-        
-        y = self.colliding_elements[0]
-        max = len(self.colliding_elements)
-        i = 0
-        while i < max:
-            function = self.functions[i]
-            if self.compute_function(function, x) != self.compute_function(function, y):
-                del self.functions[i]
-                max -= 1
-            else:
-                i += 1
-        
-        self.colliding_elements.append(x)
-        
+    def get_all_function_count(self): 
+        return self.p - 1
