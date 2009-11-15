938692095ae2a37bacfc285049d85d45fdecbc4d
Martin Babka <babky@users.noreply.github.com>
2009-11-15 16:56:07 +0000

    * FIX: Test a balicky.

--- test-linear/src/tests/cwlf/collision_functions/collision_functions_test.py
+++ test-linear/src/tests/cwlf/collision_functions/collision_functions_test.py
@@ -4,7 +4,7 @@ import unittest
 class CollisionFunctionsTest(unittest.TestCase):
 
     def test_collision_function_count(self):
-        cf = CWLFSystem(17)
+        cf = CWLFSystem(17, 13)
         self.assertEqual(cf.compute_function((3, 15), 2), 4, "CWLF function computation test.")
 
         cf.restrict_collision_functions(1)
