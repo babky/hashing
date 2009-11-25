aad8436f2e0e23de25760ddce481b72a8e44ac3c
Martin Babka <babky@users.noreply.github.com>
2009-11-25 07:41:07 +0000

    * FIX: Prikaz na kompilaciu. Zmena adresarov.

--- text/command/compile.cmd
+++ text/command/compile.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/docs/text/command/compile.sh
-"C:\Program Files\Foxit Reader.exe" H:\Documents\docs\text\output\text.pdf
+"C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/hash/docs/text/command/compile.sh
+"C:\Program Files\Foxit Reader.exe" ..\output\text.pdf
