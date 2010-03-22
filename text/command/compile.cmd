cd40fb3febc6f02d60ffe9e426da5b4baddfc4a8
Martin Babka <babky@users.noreply.github.com>
2010-03-22 14:16:44 +0000

    * Zlepsenie lingebry,\n+ Pridanie afin. podpr.\n+ Pocet rieseni sustavy.

--- text/command/compile.cmd
+++ text/command/compile.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/hash/docs/text/command/compile.sh
-"C:\Program Files\Foxit Reader.exe" ..\output\text.pdf
+..\output\text.pdf
