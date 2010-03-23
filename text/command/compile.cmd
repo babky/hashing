5447b65b5d729696613332ea01e38c6a96d85d04
babka <babka@localhost>
2010-03-23 17:47:37 +0000

    + Zlepsenia, name clash, opravy, dorobene modely a fakt z LA.

--- text/command/compile.cmd
+++ text/command/compile.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files\Portable PuTTY\plink.exe" babky@192.168.65.128 -pw babkypasswd /home/babky/Documents/hash/docs/text/command/compile.sh
-..\output\text.pdf
+"C:\Program Files\Foxit Reader.exe" h:\documents\hash\docs\text\output\text.pdf
