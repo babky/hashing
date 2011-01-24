950ec143a5845ca9e1835d5be47c4013b5a96b9c
babky-regular <babky-regular@heliodor>
2011-01-24 18:41:16 +0100

    + Komentare.

--- universal/hash/table-lib/systems/universal_system.h
+++ universal/hash/table-lib/systems/universal_system.h
@@ -9,18 +9,54 @@ namespace Hash { namespace Systems {
 	template<typename T>
 	class UniversalSystem {
 	public:
+		/**
+		 * Resets the function - chooses another from the universal system.
+		 */
 		virtual void reset(void) = 0;
 
+		/**
+		 * Current length of the hash table.
+		 *
+		 * @return The length of the hash table which is supposed by the current hash function.
+		 */
 		virtual size_t getLength(void) const = 0;
-
+		
+		/**
+		 * Sets the length of the table which will be used by the current hash function.
+		 *
+		 *
+		 * @param length New length of the table.
+		 */
 		virtual void setLength(size_t length) = 0;
 
+		/**
+		 * Maximum value of the universe.
+		 *
+		 * @param Maximal possible value.
+		 */
 		virtual size_t getUniversumMax(void) const = 0;
 
+		/**
+		 * Maximum value of the universe.
+		 *
+		 * @param Maximal possible value.
+		 */
 		virtual void setUniversumMax(size_t universumMax) = 0;
 
+		/**
+		 * Hash value of the element x
+		 *
+		 * @param x The hashed element.
+		 * @param length Length of the table.
+		 */
 		virtual size_t hash(const T & x, size_t length) = 0;
 
+		/**
+		 * Hash value of the element x
+		 *
+		 * @param x The hashed element.
+		 * @param length Length of the table.
+		 */
 		virtual size_t operator()(const T & a, size_t length) = 0;
 	};
 
