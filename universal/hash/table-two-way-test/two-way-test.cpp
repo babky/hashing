7df3c1b75a59231ae3f6a79ab4c4e1a9e3587f33
Martin Babka <babky@users.noreply.github.com>
2011-01-24 16:30:20 +0100

    - Premovovany subor do utils.

--- universal/hash/table-two-way-test/two-way-test.cpp
+++ universal/hash/table-two-way-test/two-way-test.cpp
@@ -1,4 +1,6 @@
 #include "storages/collision_count_storage.h"
+#include "systems/two_way_system.h"
+#include "systems/cwlf_system.h"
 #include "table.h"
 #include "function.h"
 #include "universal_systems.h"
@@ -13,21 +15,25 @@
 #include "default_test.h"
 #include <iostream>
 
-int main(int, const char **) {
-	using namespace Hash;
-	using namespace Hash::Utils;
-	using namespace std;
+using namespace Hash;
+using namespace Hash::Utils;
+using namespace Hash::Systems;
+using namespace std;
 
-	Table<int, ConstantComparer<int>, Function, Hash::Storages::CollisionCountStorage> t(32);
-	t.insert(1);
-	t.insert(1);
-	t.insert(1);
-	t.insert(1);
-	t.insert(1);
+template <typename T>
+class TwoWaySystemCWLF : public Hash::Systems::TwoWaySystem<T, Hash::UniversalFunctionCWLF> {
+};
+
+int main(int, const char **) {
+	const size_t TABLE_SIZE = 100000;
+	Table<int, ConstantComparer<int>, TwoWaySystemCWLF, Hash::Storages::CollisionCountStorage> t(TABLE_SIZE);
+	
+	for (size_t i = 0; i < TABLE_SIZE; ++i) {
+		t.insert(i);
+	}
 
 	StorageStatistics s;
 	t.computeStatistics(s);
 	std::cout << s;
-
 	return 0;
 }
