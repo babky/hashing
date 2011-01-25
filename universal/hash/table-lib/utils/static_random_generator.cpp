913d97e29063a45b3af51f1a0f05923ba7e704df
babky-regular <babky-regular@heliodor>
2011-01-25 18:46:46 +0100

    + Generickejsie generatory a pre viac typov.

--- universal/hash/table-lib/utils/static_random_generator.cpp
+++ universal/hash/table-lib/utils/static_random_generator.cpp
@@ -3,3 +3,6 @@
 using namespace Hash::Utils;
 
 RandomGenerator<size_t> * StaticRandomGenerator<size_t>::generator = 0;
+RandomGenerator<int> * StaticRandomGenerator<int>::generator = 0;
+RandomGenerator<boost::uint_fast64_t> * StaticRandomGenerator<boost::uint_fast64_t>::generator = 0;
+RandomGenerator<boost::int_fast64_t> * StaticRandomGenerator<boost::int_fast64_t>::generator = 0;
