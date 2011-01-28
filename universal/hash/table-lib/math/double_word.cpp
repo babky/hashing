ed41260aa3491e09ce315ec4ca782937e72e0029
babky-regular <babky-regular@heliodor>
2011-01-28 22:17:07 +0100

    + 64b podporujuci polynomialny system :)!!! * Testy idu na 64b polynomialny system.

---
+++ universal/hash/table-lib/math/double_word.cpp
@@ -0,0 +1,20 @@
+#include "double_word.h"
+#include <boost/config.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(disable: 4512 4127 4100)
+#endif
+#include <boost/integer_traits.hpp>
+#include <boost/integer.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(default: 4512 4127 4100)
+#endif
+
+using namespace Hash::Math;
+
+template<>
+const size_t Prime<size_t>::GREATEST_PRIME = 4294967291;
+
+template<>
+const boost::uint_fast64_t Prime<boost::uint_fast64_t>::GREATEST_PRIME = 18446744073709551557;
+
+
