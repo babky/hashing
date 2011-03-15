82676f3b097bb3f40db2896ce750ca8d25de2cc1
babka <babka@localhost>
2011-03-15 08:19:45 +0100

    - Command prikazy netreba.

--- universal/text/text/command/compile.cmd
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Portable PuTTY\plink.exe" -batch babky@192.168.65.128 -pw babkypasswd -m "H:\sources\hash\docs\text\command\commands.txt"
-"C:\Program Files\Foxit Reader\Foxit Reader.exe" h:\sources\hash\docs\text\output\text.pdf
