b9b7f49115876f96c4c72f2ee159f94173460e97
Martin Babka <babky@users.noreply.github.com>
2011-04-05 19:19:16 +0200

    * FIX: Upravy kodu a konfiguracie pre haf.

--- universal/hash/table-lib/math/double_word.cpp
+++ universal/hash/table-lib/math/double_word.cpp
@@ -16,7 +16,11 @@ using namespace Hash::Math;
 template<>
 const size_t Prime<size_t>::GREATEST_PRIME = boost::integer_traits<size_t>::const_max - 4;
 
+#if __LP64__ != 1
+
 template<>
 const boost::uint_fast64_t Prime<boost::uint_fast64_t>::GREATEST_PRIME = boost::integer_traits<boost::uint_fast64_t>::const_max - 58;
+
+#endif
 
 
