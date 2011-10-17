559c4c8231dc83cad571a7f67acf3281435f7218
babka <babka@localhost>
2011-10-17 13:54:39 +0200

    BitString system.

---
+++ src/hash/table-lib/systems/bit_string_system.h
@@ -0,0 +1,112 @@
+#ifndef BIT_STRING_SYSTEM_H
+#define BIT_STRING_SYSTEM_H
+
+
+#include <boost/config.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(disable: 4512 4127 4100)
+#endif
+#include <boost/integer.hpp>
+#include <boost/integer_traits.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(default: 4512 4127 4100)
+#endif
+
+#include "systems/universal_system.h"
+#include "utils/static_random_generator.h"
+
+namespace Hash { namespace Systems {
+
+	template<typename T>
+	class BitStringFunction : public UniversalSystem<T> {
+	public:
+		static const size_t START_LENGTH = 16;
+
+		~BitStringFunction(void) {
+			delete [] coefficients;
+		}
+
+		explicit BitStringFunction(size_t length = START_LENGTH):
+		  coefficients(new size_t[COEFFICIENT_NUMBER])
+		{
+			setTableSize(length);
+			reset();
+		}
+
+		BitStringFunction(const BitStringFunction & r):
+		  coefficients(new size_t[COEFFICIENT_NUMBER]),
+		  tableSize(r.tableSize),
+		  mark(r.mask)
+		{
+			for (size_t i = 0; i < COEFFICIENT_NUMBER; ++i) {
+				coefficients[i] = r.coefficients[i];
+			}
+		}
+
+		BitStringFunction & operator =(const BitStringFunction & r) {
+			BitStringFunction tmp = r;
+			tmp.swap(*this);
+			return *this;
+		}
+
+		void reset(void) {
+			for (size_t i = 0; i < COEFFICIENT_NUMBER; ++i) {
+				coefficients[i] = Hash::Utils::StaticRandomGenerator<size_t>::getGenerator().generate();
+			}
+		}
+
+		size_t getTableSize(void) const {
+			return tableSize;
+		}
+		
+		void setTableSize(size_t size) {
+			tableSize = size;
+			
+			mask = 0;
+			for (size_t i = 1; size > i; i <<= 1) {
+				mask |= i;
+			}
+		}
+
+		T getUniversumMax(void) const {
+			return boost::integer_traits<T>::const_max;
+		}
+
+		void setUniversumMax(T universumMax) {
+		}
+
+		size_t hash(const T & x, size_t length) { 
+			T coefficientMask = 1;
+			size_t result = 0;
+
+			for (size_t i = 0; i < COEFFICIENT_NUMBER; ++i) {
+				result ^= (x & coefficientMask) * coefficients[i];
+				coefficientMask <<= 1;
+			}
+
+			return result & mask;
+		}
+
+		size_t operator()(const T & a, size_t length) {
+			return hash(a, length);
+		}
+
+		void swap(BitStringFunction & r) {
+			using std::swap;
+
+			swap(this->coefficients, r.coefficients);
+			swap(this->tableSize, r.tableSize);
+			swap(this->mask, r.mask);
+		}
+
+	private:
+		static const size_t COEFFICIENT_NUMBER = boost::integer_traits<T>::digits;
+
+		size_t * coefficients;
+		size_t tableSize;
+		size_t mask;
+	};
+
+} }
+
+#endif /* BIT_STRING_SYSTEM_H */
