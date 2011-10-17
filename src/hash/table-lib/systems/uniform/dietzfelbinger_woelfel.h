43762e6cf30a285468f3426df42c70fa22a13451
Martin Babka <babky@users.noreply.github.com>
2011-10-17 13:54:28 +0200

    Copy - ctor pre DW system.

--- src/hash/table-lib/systems/uniform/dietzfelbinger_woelfel.h
+++ src/hash/table-lib/systems/uniform/dietzfelbinger_woelfel.h
@@ -1,6 +1,7 @@
 #ifndef DIETZFELBINGER_WOELFEL_H
 #define DIETZFELBINGER_WOELFEL_H
 
+#include <algorithm>
 #include "systems/universal_system.h"
 #include "systems/universal_system_factory.h"
 #include "utils/hash_assert.h"
@@ -12,9 +13,11 @@ namespace Hash { namespace Systems { namespace Uniform {
 	class DietzfelbingerWoelfel : public UniversalSystem<T> {
 	public:
 		const static size_t START_LENGTH = 16;
+
+		// TODO: Figure this out.
 		// const static double DEFAULT_R_EXPONENT = 0.75;
 
-		virtual ~DietzfelbingerWoelfel(void) {
+		~DietzfelbingerWoelfel(void) {
 			delete [] z;
 		}
 
@@ -27,7 +30,26 @@ namespace Hash { namespace Systems { namespace Uniform {
 			reset();
 		}
 
-		virtual void reset(void) {
+		DietzfelbingerWoelfel(const DietzfelbingerWoelfel & a):
+		  length(a.length),
+		  r(a.r),
+		  rExponent(a.rExponent),
+		  z(new size_t[a.r]),
+		  f(a.f),
+		  g(a.g)
+		{
+			for (size_t i = 0; i < r; ++i) {
+				z[i] = a.z[i];
+			}
+		}
+
+		DietzfelbingerWoelfel & operator=(const DietzfelbingerWoelfel & r) {
+			DietzfelbingerWoelfel tmp = r;
+			tmp.swap(*this);
+			return *this;
+		}
+
+		void reset(void) {
 			for (size_t i = 0; i < r; ++i) {
 				z[i] = Hash::Utils::StaticRandomGenerator<T>::getGenerator().generate();
 			}
@@ -36,34 +58,45 @@ namespace Hash { namespace Systems { namespace Uniform {
 			g = UniversalSystemFactory<T, HashFunction>::create(r);
 		}
 
-		virtual size_t getTableSize(void) const {
+		size_t getTableSize(void) const {
 			return length;
 		}
 		
-		virtual void setTableSize(size_t size) {
+		void setTableSize(size_t size) {
 			length = size;
 			resetRandomVector();
 			f.setTableSize(size);
 			g.setTableSize(size);
 		}
 
-		virtual T getUniversumMax(void) const {
+		T getUniversumMax(void) const {
 			return f.getUniversumMax();
 		}
 
-		virtual void setUniversumMax(T universumMax) {
+		void setUniversumMax(T universumMax) {
 			f.setUniversumMax(universumMax);
 			g.setUniversumMax(universumMax);
 		}
 
-		virtual size_t hash(const T & x, size_t length) {
+		size_t hash(const T & x, size_t length) {
 			simple_assert(length == this->length, "Lengths must be the same.");
 
 			return (f(x, length) + z[g(x, r)]) % length;
 		}
 
-		virtual size_t operator()(const T & a, size_t length) {
+		size_t operator()(const T & a, size_t length) {
 			return hash(a, length);
+		}
+
+		void swap(DietzfelbingerWoelfel & r) {
+			using std::swap;
+
+			swap(this->length, r.length);
+			swap(this->r, r.r);
+			swap(this->rExponent, r.rExponent);
+			swap(this->z, r.z);
+			swap(this->f, r.f);
+			swap(this->g, r.g);
 		}
 
 	protected:
