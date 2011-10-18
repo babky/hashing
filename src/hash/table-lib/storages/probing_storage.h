ae225798d9d7ad5bb6e58a7ee1de9dcb4609eccc
Martin Babka <babky@users.noreply.github.com>
2011-10-18 02:25:22 +0200

    Fix - EC - zly namespace.

--- src/hash/table-lib/storages/probing_storage.h
+++ src/hash/table-lib/storages/probing_storage.h
@@ -341,7 +341,7 @@ namespace Hash { namespace Storages {
 		LinearProbingStorage(void) {
 		}
 
-		explicit LinearProbingStorage(const EqualityComparer & comparer, 
+		explicit LinearProbingStorage(const Hash::Utils::EqualityComparer & comparer, 
 			size_t tableLength = StorageParams::STARTING_STORAGE_SIZE):
 		  ProbingStorage(comparer, tableLength)
 		{
@@ -356,7 +356,7 @@ namespace Hash { namespace Storages {
 		QuadraticProbingStorage(void) {
 		}
 
-		explicit QuadraticProbingStorage(const EqualityComparer & comparer, 
+		explicit QuadraticProbingStorage(const Hash::Utils::EqualityComparer & comparer, 
 			size_t tableLength = StorageParams::STARTING_STORAGE_SIZE):
 		  ProbingStorage(comparer, tableLength)
 		{
