771560c4449c7692edaf42fe06064e8e487268d2
Martin Babka <babky@users.noreply.github.com>
2011-11-03 18:10:26 +0100

    Testovanie vykonu funkcii.

--- src/hash/system-test/system_test.cpp
+++ src/hash/system-test/system_test.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <boost/config.hpp>
+#include <boost/program_options.hpp>
 #ifdef BOOST_MSVC
 	#pragma warning(disable: 4512 4127 4100)
 #endif
@@ -17,6 +18,7 @@
 #include "systems/tabulation_system.h"
 #include "systems/polynomial_system.h"
 #include "systems/tr1_function.h"
+#include "systems/identity_function.h"
 
 using namespace std;
 using namespace boost::posix_time;
@@ -34,54 +36,103 @@ namespace Hash { namespace Systems {
 
 } }
 
-template<typename T, class Storage>
-struct SystemDescription {
-	SystemDescription(Hash::Systems::UniversalFunction<T, Storage> * f, string n):
-	  function(f),
+struct Test {
+	Test(string n):
 	  name(n) {
 	}
 
-	Hash::Systems::UniversalFunction<T, Storage> * function;
+	virtual void run(void) = 0;
+
+protected:
 	string name;
 	time_duration time;
 };
 
-int main(void) {
-	// typedef size_t T;
-	typedef boost::uint64_t T;
+template<class F, typename T>
+struct FunctionTest : public Test {
+
+	FunctionTest(size_t aLength, string name):
+	  Test(name),
+	  length(aLength) {
+	}
+
+	virtual void run() {
+		function.setTableSize(length);
+		function.reset();
+
+		ptime start = microsec_clock::local_time();
+		size_t hash = 0xdead;
+		for (size_t i = 0; i < length; ++i) {
+			hash ^= function.hash(i);
+		}
+		ptime finish = microsec_clock::local_time();
+		time = finish - start;
+
+		if (hash == 0) {
+			cout << "Almost impossible :)!\n";
+		}
+		
+		cout << name << ": " << time.total_milliseconds() << " ms" << endl;
+	}
+
+private:
+	size_t length;
+	F function;
+};
+
+template<typename T>
+void runTest(size_t length) {
 	typedef size_t TestStorage;
-	typedef SystemDescription<T, TestStorage> TestSystemDescription;
-	typedef vector<TestSystemDescription> FunctionVector;
-#ifdef HASH_DEBUG
-	const size_t TABLE_SIZE = 1 << 10;
-#else
-	const size_t TABLE_SIZE = 1 << 24;
-#endif
+	typedef vector<Test *> FunctionVector;
+	using namespace Hash::Systems;
 
 	FunctionVector functions;
 	
-	functions.push_back(TestSystemDescription(new Hash::Systems::UniversalFunctionCWLF<T, TestStorage>(TABLE_SIZE), "CWLF"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::Tr1Function<T, TestStorage>(TABLE_SIZE), "TR1"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::BitStringFunction<T, TestStorage>(TABLE_SIZE), "BitString"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::TabulationFunction<T, TestStorage>(TABLE_SIZE), "Tabulation"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::UniversalFunctionLinearMap<T, TestStorage>(TABLE_SIZE), "LinearMap"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::PolynomialSystem<T, TestStorage>(TABLE_SIZE), "Polynomial - deg 2"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::Uniform::DietzfelbingerWoelfel<T, TestStorage, Hash::Systems::PolynomialSystem4>(TABLE_SIZE), "DW - deg 4"));
-	functions.push_back(TestSystemDescription(new Hash::Systems::PolynomialSystem<T, TestStorage>(TABLE_SIZE, Hash::Math::Prime<T>::GREATEST_PRIME, 32), "Polynomial - deg 32"));
-	
-	for (FunctionVector::iterator b = functions.begin(), e = functions.end(); b != e; ++b) {
-		b->function->setTableSize(TABLE_SIZE);
-		b->function->reset();
+	functions.push_back(new FunctionTest<IdentityFunction<T, TestStorage>, T>(length, "ID"));
+	functions.push_back(new FunctionTest<Tr1Function<T, TestStorage>, T>(length, "TR1"));
+	functions.push_back(new FunctionTest<UniversalFunctionCWLF<T, TestStorage>, T>(length, "CWLF"));
+	functions.push_back(new FunctionTest<BitStringFunction<T, TestStorage>, T>(length, "BitString"));
+	functions.push_back(new FunctionTest<TabulationFunction<T, TestStorage>, T>(length, "Tabulation"));
+	functions.push_back(new FunctionTest<UniversalFunctionLinearMap<T, TestStorage>, T>(length, "LinearMap"));
+	functions.push_back(new FunctionTest<PolynomialSystem<T, TestStorage>, T>(length, "Polynomial - deg 2"));
+	functions.push_back(new FunctionTest<PolynomialSystem4<T, TestStorage>, T>(length, "Polynomial - deg 4"));
+	functions.push_back(new FunctionTest<Uniform::DietzfelbingerWoelfel<T, TestStorage, Hash::Systems::PolynomialSystem4>, T>(length, "DW - deg 4"));
 
-		ptime start = microsec_clock::local_time();
-		for (size_t i = 0; i < TABLE_SIZE / 2; ++i) {
-			b->function->hash(i);
-		}
-		ptime finish = microsec_clock::local_time();
-		b->time = finish - start;
-		
-		cout << b->name << ": " << b->time.total_milliseconds() << " ms" << endl;
+	for (FunctionVector::iterator b = functions.begin(), e = functions.end(); b != e; ++b) {
+		(*b)->run();
 	}
+}
+
+int main(int argc, char ** argv) {
+	using namespace boost::program_options;
+
+	size_t bits;
+	const size_t DEFAULT_BITS = 32;
+
+	size_t length;
+#ifdef HASH_DEBUG
+	const size_t DEFAULT_LENGTH = 10;
+#else
+	const size_t DEFAULT_LENGTH = 24;
+#endif
+
+	options_description opts("Universal systems test options.");
+	opts.add_options()
+		("bits", value<size_t>(&bits)->default_value(DEFAULT_BITS), "the number of bits used 32 or 64")
+		("length", value<size_t>(&length)->default_value(DEFAULT_LENGTH), "the logarithm of number of times the function is called and the size of the table");
+		
+	variables_map vm;
+	store(parse_command_line(argc, argv, opts), vm);
+	notify(vm);
 	
+	if (bits == 32) {
+		runTest<boost::uint32_t>(1 << length);
+	} else if (bits == 64) {
+		runTest<boost::uint64_t>(1 << length);
+	} else {
+		std::cerr << opts;
+	}
+
 	return 0;
 }
+
