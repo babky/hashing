7bb666ce1c2af7963328c36469a25f24a39fc86a
babka <babka@localhost>
2011-11-18 16:10:14 +0100

    Opravy linkovania.

--- src/hash/table-lib/utils/static_random_generator.cpp
+++ src/hash/table-lib/utils/static_random_generator.cpp
@@ -3,7 +3,7 @@
 using namespace Hash::Utils;
 
 template<>
-RandomGenerator<size_t> * StaticRandomGenerator<size_t>::generator = 0;
+RandomGenerator<boost::uint32_t> * StaticRandomGenerator<boost::uint32_t>::generator = 0;
 
 template<>
 RandomGenerator<int> * StaticRandomGenerator<int>::generator = 0;
@@ -11,12 +11,6 @@ RandomGenerator<int> * StaticRandomGenerator<int>::generator = 0;
 template<>
 RandomGenerator<unsigned char> * StaticRandomGenerator<unsigned char>::generator = 0;
 
-#if __LP64__ != 1
-
 template<>
-RandomGenerator<boost::uint_fast64_t> * StaticRandomGenerator<boost::uint_fast64_t>::generator = 0;
+RandomGenerator<boost::uint64_t> * StaticRandomGenerator<boost::uint64_t>::generator = 0;
 
-template<>
-RandomGenerator<boost::int_fast64_t> * StaticRandomGenerator<boost::int_fast64_t>::generator = 0;
-
-#endif
