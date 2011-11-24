6686d0a6b9423fe0ca0ebf7815f1e23d04838901
Vladim?r ?un?t <vcunat@matfyz.cz>
2011-11-24 19:52:48 +0100

    Virtualita - smazani i u identity

--- src/hash/table-lib/systems/identity_function.h
+++ src/hash/table-lib/systems/identity_function.h
@@ -1,7 +1,16 @@
 #ifndef IDENTITY_FUNCTION
 #define IDENTITY_FUNCTION
 
-#include "../utils/boost_include.h"
+#include <boost/config.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(disable: 4512 4127 4100)
+#endif
+#include <boost/integer.hpp>
+#include <boost/integer_traits.hpp>
+#ifdef BOOST_MSVC
+	#pragma warning(default: 4512 4127 4100)
+#endif
+
 #include "universal_system.h"
 #include <cstdlib>
 #include <algorithm>
@@ -18,26 +27,26 @@ namespace Hash { namespace Systems {
 		void reset(void) {
 		}
 
-		virtual size_t getTableSize(void) const {
+		size_t getTableSize(void) const {
 			return mask + 1;
 		}
 		
-		virtual void setTableSize(size_t size) {
+		void setTableSize(size_t size) {
 			mask = size - 1;
 		}
 
-		virtual T getUniversumMax(void) const {
+		T getUniversumMax(void) const {
 			return boost::integer_traits<T>::const_max;
 		}
 
-		virtual void setUniversumMax(T) {
+		void setUniversumMax(T) {
 		}
 
-		virtual size_t hash(const T & x) {
+		size_t hash(const T & x) {
 			return x & mask;
 		}
 
-		virtual size_t operator()(const T & a) {
+		size_t operator()(const T & a) {
 			return hash(a);
 		}
 
