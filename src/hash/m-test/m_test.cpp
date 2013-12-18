cb1b39f3d1cf6492e646845c26cd97e1d802195a
babky <babky@localhost>
2013-12-18 10:36:48 +0100

    Added the random tests.

--- src/hash/m-test/m_test.cpp
+++ src/hash/m-test/m_test.cpp
@@ -1,5 +1,6 @@
 #include "storages/chained_storage.h"
 #include "systems/cwlf_system.h"
+#include "systems/random_system.h"
 #include "table.h"
 #include "utils/equality_comparer.h"
 #include "utils/storage_statistics.h"
@@ -13,9 +14,8 @@
 	#pragma warning(default: 4512)
 #endif
 
-
-int main(int argc, const char ** argv) {
-	// Definitions.
+template <typename Table>
+void performTest(size_t runs, size_t m) {
 	using namespace Hash;
 	using namespace Hash::Storages;
 	using namespace Hash::Systems;
@@ -23,38 +23,13 @@ int main(int argc, const char ** argv) {
 
 	using namespace std;
 
-	using namespace boost;
-	using namespace boost::program_options;
-
-	typedef Table<size_t, EqualityComparer<size_t>, UniversalFunctionCWLF, ChainedStorage> HashTable;
-
-	// Parse the command line.
-	const size_t DEFAULT_M = 65536;
-	const size_t DEFAULT_RUNS = 64;
-
-	size_t m = DEFAULT_M;
-	size_t runs = DEFAULT_RUNS;
-
-	options_description optsDesc("Table Two Way Test allowed options.");
-	optsDesc.add_options()
-		("help", "prints this help message")
-		("m", value<size_t>(&m)->default_value(DEFAULT_M), "The m.")
-		("runs", value<size_t>(&runs)->default_value(DEFAULT_RUNS), "The number of runs.");
-
-	variables_map vm;
-	store(parse_command_line(argc, argv, optsDesc), vm);
-	notify(vm);
-
-	if (vm.count("help")) {
-		cout << optsDesc;
-		return 0;
-	}
-
 	size_t max = 0, min = m, sum = 0;
+
+	Table t;
 
 	// Run it.
 	for (size_t run = 0; run < runs; ++run) {
-		HashTable t;
+		t = Table();
 		for (size_t i = 0; i < m; ++i) {
 			t.insert(i);
 		}
@@ -77,6 +52,56 @@ int main(int argc, const char ** argv) {
 	cout << "MIN:\t" << min << "\n";
 	cout << "AVG:\t" << ((double) sum) / runs << "\n";
 	cout << "MAX:\t" << max << endl;
+}
+
+int main(int argc, const char ** argv) {
+	// Definitions.
+	using namespace Hash;
+	using namespace Hash::Storages;
+	using namespace Hash::Systems;
+	using namespace Hash::Utils;
+
+	using namespace std;
+
+	using namespace boost;
+	using namespace boost::program_options;
+
+	typedef Table<size_t, EqualityComparer<size_t>, RandomBin, ChainedStorage> HashTableRandom;
+	typedef Table<size_t, EqualityComparer<size_t>, UniversalFunctionCWLF, ChainedStorage> HashTableCWLF;
+
+	// Parse the command line.
+	const size_t DEFAULT_M = 65536;
+	const size_t DEFAULT_RUNS = 64;
+	const string DEFAULT_FUNCTION = "cwlf";
+
+	size_t m = DEFAULT_M;
+	string functionType = DEFAULT_FUNCTION;
+	size_t runs = DEFAULT_RUNS;
+
+	options_description optsDesc("Table Two Way Test allowed options.");
+	optsDesc.add_options()
+		("help", "prints this help message")
+		("m", value<size_t>(&m)->default_value(DEFAULT_M), "The m.")
+		("runs", value<size_t>(&runs)->default_value(DEFAULT_RUNS), "The number of runs.")
+		("function", value<string>(&functionType)->default_value(DEFAULT_FUNCTION), "Default function cwlf, random.");
+
+	variables_map vm;
+	store(parse_command_line(argc, argv, optsDesc), vm);
+	notify(vm);
+
+	if (vm.count("help")) {
+		cout << optsDesc;
+		return 0;
+	}
+
+	if (functionType == "cwlf") {
+		performTest<HashTableCWLF>(runs, m);
+	} else if (functionType == "random") {
+		performTest<HashTableRandom>(runs, m);
+	} else {
+		cerr << "What the function? Use random or cwlf." << endl;
+		return 1;
+	}
 
 	return 0;
 }
--- src/hash/table-lib/math/double_word.cpp
+++ src/hash/table-lib/math/double_word.cpp
@@ -6,7 +6,7 @@ using namespace Hash::Math;
 // Primes taken from http://primes.utm.edu/lists/2small/0bit.html.
 
 template<>
-const boost::uint32_t Prime<boost::uint32_t>::GREATEST_PRIME = boost::integer_traits<boost::uint32_t>::const_max - 4;
+const boost::uint32_t Prime<boost::uint32_t>::GREATEST_PRIME = ((size_t) 1 << 31) - 1; // boost::integer_traits<boost::uint32_t>::const_max - 4;
 
 template<>
 const boost::uint64_t Prime<boost::uint64_t>::GREATEST_PRIME = boost::integer_traits<boost::uint64_t>::const_max - 58;
