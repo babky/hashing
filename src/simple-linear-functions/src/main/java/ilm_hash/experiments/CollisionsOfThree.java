24868a6a447617a08048ecab0c33d1d452e49f3d
Martin Babka <babky@users.noreply.github.com>
2014-11-24 10:25:07 +0100

    Refactor and bad set test.

--- src/simple-linear-functions/src/main/java/ilm_hash/experiments/CollisionsOfThree.java
+++ src/simple-linear-functions/src/main/java/ilm_hash/experiments/CollisionsOfThree.java
@@ -1,5 +1,7 @@
 package ilm_hash.experiments;
 
+import ilm_hash.experiments.utils.ThreeElementsCollisionProbabilityComputer;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -16,79 +18,8 @@ public class CollisionsOfThree {
 
 	static final int M = 64;
 
-	static class Test {
-
-		public Test(int p, int m, int d) {
-			this.m = m;
-			this.p = p;
-			this.d = d;
-		}
-
-		public long perform() {
-			long res = 0;
-			for (int b = 0; b < p; ++b) {
-				int a = 0;
-				// Case 1: l1 == 0. a + b < p. m | a.
-				while (a + b < p) {
-					if (((a + b) % m == b % m) && (((a * d + b) % p) % m == b % m)) {
-						++res;
-					}
-					
-					a += m;
-				}
-				
-				// Case 2: l1 == 1. a + b >= p. m | p - a.
-				a = p - (p % m);
-				while (a + b >= p) {
-					if (((a + b) % m == b % m) && (((a * d + b) % p) % m == b % m)) {
-						++res;
-					}
-					
-					a -= m;
-				}
-			}
-
-			return res;
-		}
-
-		public long getSpaceSize() {
-			return p * p;
-		}
-
-		public int getTableSize() {
-			return m;
-		}
-
-		public void setTableSize(int m) {
-			this.m = m;
-		}
-
-		public int getPrime() {
-			return p;
-		}
-
-		public void setPrime(int p) {
-			this.p = p;
-		}
-
-		public int getMaxElement() {
-			return d;
-		}
-
-		public void setMaxElement(int d) {
-			this.d = d;
-		}
-
-		int m;
-
-		int p;
-
-		int d;
-
-	}
-
 	public static void main(String[] args) throws IOException {
-		Test t = new Test(P, M, 0);
+		ThreeElementsCollisionProbabilityComputer t = new ThreeElementsCollisionProbabilityComputer(P, M, 0);
 		String fname = String.format("coll-3-%d-%d.png", t.getPrime(), t.getTableSize());
 		String fname_small = String.format("coll-3-%d-%d.png", t.getPrime(), t.getTableSize());
 
@@ -111,7 +42,7 @@ public class CollisionsOfThree {
 			}
 
 			t.setMaxElement(d);
-			dataset.add(d, (double) t.perform() / ((double) t.getSpaceSize()));
+			dataset.add(d, (double) t.estimate() / ((double) t.getSpaceSize()));
 		}
 		System.out.println("Done, outputting graph.");
 
