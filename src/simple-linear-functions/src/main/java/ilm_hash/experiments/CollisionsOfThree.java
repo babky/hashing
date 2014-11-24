dd8dbd8981ed83ceb804723dffc3399ad2baa696
babka <babka@localhost>
2014-11-24 14:28:21 +0100

    Output to err not to out.

--- src/simple-linear-functions/src/main/java/ilm_hash/experiments/CollisionsOfThree.java
+++ src/simple-linear-functions/src/main/java/ilm_hash/experiments/CollisionsOfThree.java
@@ -5,6 +5,11 @@ import ilm_hash.experiments.utils.ThreeElementsCollisionProbabilityComputer;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
@@ -14,27 +19,40 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class CollisionsOfThree {
 
-	static final int P = 9851;
+	static final int DEFAULT_PRIME = 9851;
 
-	static final int M = 64;
+	static final int DEFAULT_TABLE_SIZE = 64;
 
-	public static void main(String[] args) throws IOException {
-		ThreeElementsCollisionProbabilityComputer t = new ThreeElementsCollisionProbabilityComputer(P, M, 0);
-		String fname = String.format("coll-3-%d-%d.png", t.getPrime(), t.getTableSize());
-		String fname_small = String.format("coll-3-%d-%d.png", t.getPrime(), t.getTableSize());
+	public static void main(String[] args) throws IOException, ParseException {
+		int p;
+		int m;
+
+		CommandLineParser parser = new BasicParser();
+		Options options = new Options();
+		options.addOption("m", "table-size", true, "The table size.");
+		options.addOption("p", "prime", true, "Prime.");
+		CommandLine cmd = parser.parse(options, args);
+		p = Integer.parseInt(cmd.getOptionValue("p", Integer.toString(DEFAULT_PRIME)));
+		m = Integer.parseInt(cmd.getOptionValue("m", Integer.toString(DEFAULT_TABLE_SIZE)));
+
+		ThreeElementsCollisionProbabilityComputer t = new ThreeElementsCollisionProbabilityComputer(p, m, 0);
+		String fname_full = String.format("coll-3-%d-%d.png", t.getPrime(), t.getTableSize());
+		String fname_scaled = String.format("coll-3-%d-%d-scaled.png", t.getPrime(), t.getTableSize());
+		String fname_small = String.format("coll-3-%d-%d-%d.png", t.getPrime(), t.getTableSize(),
+				t.getPrime() / t.getTableSize());
 
 		XYSeries dataset = new XYSeries("Probabilty");
 		dataset.setMaximumItemCount(t.getPrime());
 
 		int percent = -1;
-		for (int d = 0; d < t.getPrime(); ++d) {
+		for (int d = 1; d < t.getPrime(); ++d) {
 			if (d * 100 / t.getPrime() > percent) {
 				percent = d * 100 / t.getPrime();
-				System.out.println(String.format("%2d %%", percent));
+				System.err.println(String.format("%2d %%", percent));
 			}
-			
+
 			if (d == (t.getPrime() / t.getTableSize())) {
-				System.out.println("Outputting small graph.");
+				System.err.println("Outputting small graph.");
 				JFreeChart chart = ChartFactory.createXYLineChart("Probability of collision dependent on d", "d", "P",
 						new XYSeriesCollection(dataset), PlotOrientation.VERTICAL, true, true, false);
 
@@ -44,11 +62,12 @@ public class CollisionsOfThree {
 			t.setMaxElement(d);
 			dataset.add(d, (double) t.estimate() / ((double) t.getSpaceSize()));
 		}
-		System.out.println("Done, outputting graph.");
+		System.err.println("Done, outputting graph.");
 
 		JFreeChart chart = ChartFactory.createXYLineChart("Probability of collision dependent on d", "d", "P",
 				new XYSeriesCollection(dataset), PlotOrientation.VERTICAL, true, true, false);
 
-		ChartUtilities.saveChartAsPNG(new File(fname), chart, t.getPrime() + 100, 768);
+		ChartUtilities.saveChartAsPNG(new File(fname_full), chart, t.getPrime() + 100, 768);
+		ChartUtilities.saveChartAsPNG(new File(fname_scaled), chart, 1024, 768);
 	}
 }
