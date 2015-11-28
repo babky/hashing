3fa9f30ab2af56bb3d7cdaef2db75cff2c4e7f43
Martin Babka <babky@users.noreply.github.com>
2015-11-29 00:03:09 +0100

    Fix of the project files.

--- src/hash/cmake/cmake_eclipse.cmd
+++ src/hash/cmake/cmake_eclipse.cmd
@@ -1,1 +1,1 @@
-cmake -G "Eclipse CDT4 - MinGW Makefiles" -D "BOOST_ROOT=c:\users\martin\documents\source\boost_1_55_0" . 
+cmake -G "Eclipse CDT4 - MinGW Makefiles" -D "BOOST_ROOT=D:\source\boost_1_55_0" . 
