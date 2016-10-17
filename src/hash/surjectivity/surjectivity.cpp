ded9071962fcb91acd0872c04160e436ca997238
Martin Babka <babka@atteq.com>
2016-10-17 23:40:40 +0200

    Fix - surjectivity counter.

--- src/hash/surjectivity/surjectivity.cpp
+++ src/hash/surjectivity/surjectivity.cpp
@@ -3,9 +3,11 @@
 #include <numeric>
 #include <utility>
 #include "systems/cwlf_system.h"
+#include "systems/multiply_shift_system.h"
 #include "storages/chained_storage.h"
 #include "utils/equality_comparer.h"
 #include "table.h"
+#include <boost/multiprecision/cpp_int.hpp>
 #ifdef BOOST_MSVC
 #pragma warning(disable: 4512)
 #endif
@@ -78,8 +80,20 @@ bool is_surjective(const ElementVector & v, Function f, bool * table) {
 	return accumulate(table, table + f.getTableSize(), true, [](bool a, bool b) {return a & b;});
 }
 
+struct SurjectivityResult {
+
+	SurjectivityResult(size_t aSurjectiveCount, size_t aFunctionCount):
+		surjectiveCount(aSurjectiveCount),
+		functionCount(aFunctionCount) {
+	}
+
+	size_t surjectiveCount;
+	size_t functionCount;
+
+};
+
 template<class Function, class FunctionGenerator>
-pair<size_t, size_t> surjective_number(const ElementVector & v, FunctionGenerator g) {
+SurjectivityResult surjective_number(const ElementVector & v, FunctionGenerator g) {
 	size_t count = 0;
 	size_t surjectiveCount = 0;
 	bool * table = 0;
@@ -98,7 +112,7 @@ pair<size_t, size_t> surjective_number(const ElementVector & v, FunctionGenerato
 	delete[] table;
 	table = 0;
 
-	return make_pair(surjectiveCount, count);
+	return SurjectivityResult(surjectiveCount, count);
 }
 
 struct LongestChainResult {
@@ -116,7 +130,8 @@ struct LongestChainResult {
 
 };
 
-typedef Table<size_t, Hash::Utils::EqualityComparer<size_t>, UniversalFunctionCWLF, Hash::Storages::ChainedStorage> SimpleTable;
+typedef Table<size_t, Hash::Utils::EqualityComparer<size_t>, UniversalFunctionCWLF, Hash::Storages::ChainedStorage> TableCWLF;
+typedef Table<size_t, Hash::Utils::EqualityComparer<size_t>, MultiplyShiftSystem, Hash::Storages::ChainedStorage> TableMultiplyShift;
 
 template<class Function, class FunctionGenerator>
 LongestChainResult longest_chain(const ElementVector & v, FunctionGenerator g) {
@@ -126,7 +141,8 @@ LongestChainResult longest_chain(const ElementVector & v, FunctionGenerator g) {
 	Function f = g.next();
 	Hash::Utils::StorageStatistics stats;
 	for (;; f = g.next()) {
-		SimpleTable table(f);
+		TableCWLF table(f);
+		stats.clear();
 
 		for (size_t i = 0; i < v.size(); ++i) {
 			table.insert(v[i]);
@@ -144,17 +160,101 @@ LongestChainResult longest_chain(const ElementVector & v, FunctionGenerator g) {
 	return LongestChainResult(sum, count);
 }
 
+template<class Table, bool intermediate>
+void perform_experiment(size_t universeSize, size_t setSize, size_t tableSize) {
+	ElementVector v = first(setSize);
+	SurjectivityResult surjectivityResult(0, 0);
+	LongestChainResult longestChainResult;
+
+	bool first = true;
+	SurjectivityResult sResult(0, 0);
+	ElementVector sVector;
+	LongestChainResult lResult;
+	ElementVector lVector;
+
+	for (bool shouldContinue = true; shouldContinue; shouldContinue = next(v, universeSize - 1)) {
+		typename Table::HashFunction::Generator gs(universeSize, tableSize);
+		surjectivityResult = surjective_number<typename Table::HashFunction, typename Table::HashFunction::Generator>(v,
+				gs);
+
+		typename Table::HashFunction::Generator gl(universeSize, setSize);
+		longestChainResult = longest_chain<typename Table::HashFunction, typename Table::HashFunction::Generator>(v,
+				gl);
+
+		if (intermediate) {
+			cout << v << " " << surjectivityResult.surjectiveCount << "/" << surjectivityResult.functionCount << " "
+					<< longestChainResult.sum << "/" << longestChainResult.count << "\n";
+		}
+
+		if (first) {
+			first = false;
+			sVector = v;
+			sResult = surjectivityResult;
+
+			lVector = v;
+			lResult = longestChainResult;
+		} else {
+			if (surjectivityResult.surjectiveCount < sResult.surjectiveCount) {
+				sVector = v;
+				sResult = surjectivityResult;
+			}
+
+			if (longestChainResult.sum > lResult.sum) {
+				lVector = v;
+				lResult = longestChainResult;
+			}
+		}
+	}
+
+	cout << "Results\n";
+	cout << "Surjectivity:  " << sVector << " " << sResult.surjectiveCount << "/" << sResult.functionCount << "\n";
+	cout << "Longest chain: " << lVector << " " << lResult.sum << "/" << lResult.count << " " << boost::multiprecision::cpp_rational(lResult.sum, lResult.count).convert_to<double>() << "\n";
+}
+
+template<class Table, bool intermediate>
+void perform_partial_experiment(size_t universeSize, size_t setSize, size_t tableSize) {
+	ElementVector set;
+
+	size_t log = Hash::Math::log2exact(tableSize);
+	for (size_t i = 0; i < log; ++i) {
+		set.push_back(i);
+	}
+
+	for (size_t x = tableSize; set.size() < setSize; x += tableSize) {
+		set.push_back(x);
+	}
+
+	typename Table::HashFunction::Generator gs(universeSize, tableSize);
+	SurjectivityResult sResult = surjective_number<typename Table::HashFunction,
+			typename Table::HashFunction::Generator>(set, gs);
+
+	typename Table::HashFunction::Generator gl(universeSize, setSize);
+	LongestChainResult lResult = longest_chain<typename Table::HashFunction,
+			typename Table::HashFunction::Generator>(set, gl);
+
+	cout << "Results\n";
+	cout << "Surjectivity:  " << set << " " << sResult.surjectiveCount << "/" << sResult.functionCount << "\n";
+	cout << "Longest chain: " << set << " " << lResult.sum << "/" << lResult.count << " " << boost::multiprecision::cpp_rational(lResult.sum, lResult.count).convert_to<double>() << "\n";
+}
+
 int main(int argc, char ** argv) {
 	using namespace boost::program_options;
 
-	size_t prime = 17;
+	size_t universeSize = 17;
 	size_t setSize = 8;
 	size_t tableSize = 4;
+	bool intermediate = true;
+	bool shortExperiment = false;
+	string function = "cwlf";
 
 	options_description optsDesc("Probability distribution computation.");
-	optsDesc.add_options()("help", "prints this help message")("p", value<size_t>(&prime)->default_value(prime),
-			"Universe")("s", value<size_t>(&setSize)->default_value(setSize), "The size of the set.")("t",
-			value<size_t>(&tableSize)->default_value(tableSize), "The size of the table.");
+	optsDesc.add_options()("help,h", "prints this help message")\
+		("universe,u", value<size_t>(&universeSize)->default_value(universeSize), "Universe")\
+		("set,s", value<size_t>(&setSize)->default_value(setSize), "The size of the set.")\
+		("table,t", value<size_t>(&tableSize)->default_value(tableSize), "The size of the table.")\
+		("short-experiment,e", value<bool>(&shortExperiment)->default_value(shortExperiment), "Short experiment.")\
+		("function,f", value<string>(&function)->default_value(function), "Function type.")\
+		("intermediate,i", value<bool>(&intermediate)->default_value(intermediate), "If the intermediate output should be provided.");
 
 	variables_map vm;
 	try {
@@ -170,49 +270,19 @@ int main(int argc, char ** argv) {
 		return 0;
 	}
 
-	ElementVector v = first(setSize);
-	pair<size_t, size_t> surjectivityResult;
-	LongestChainResult longestChainResult;
-
-	bool first = true;
-	pair<size_t, size_t> sResult = make_pair(0, 0);
-	ElementVector sVector;
-	LongestChainResult lResult;
-	ElementVector lVector;
-
-	for (bool shouldContinue = true; shouldContinue; shouldContinue = next(v, prime - 1)) {
-		SimpleTable::HashFunction::Generator gs(prime, tableSize);
-		surjectivityResult = surjective_number<SimpleTable::HashFunction, SimpleTable::HashFunction::Generator>(v, gs);
-
-		SimpleTable::HashFunction::Generator gl(prime, setSize);
-		longestChainResult = longest_chain<SimpleTable::HashFunction, SimpleTable::HashFunction::Generator>(v, gl);
-
-		cout << v << " " << surjectivityResult.first << "/" << surjectivityResult.second << " "
-				<< longestChainResult.sum << "/" << longestChainResult.count << "\n";
-
-		if (first) {
-			first = false;
-			sVector = v;
-			sResult = surjectivityResult;
-
-			lVector = v;
-			lResult = longestChainResult;
+	if (intermediate) {
+		if (shortExperiment) {
+			perform_partial_experiment<TableCWLF, true>(universeSize, setSize, tableSize);
 		} else {
-			if (surjectivityResult.first < sResult.first) {
-				sVector = v;
-				sResult = surjectivityResult;
-			}
-
-			if (longestChainResult.sum > lResult.sum) {
-				lVector = v;
-				lResult = longestChainResult;
-			}
+			perform_experiment<TableCWLF, true>(universeSize, setSize, tableSize);
+		}
+	} else {
+		if (shortExperiment) {
+			perform_partial_experiment<TableCWLF, true>(universeSize, setSize, tableSize);
+		} else {
+			perform_experiment<TableCWLF, false>(universeSize, setSize, tableSize);
 		}
 	}
-
-	cout << "Results\n";
-	cout << "Surjectivity:  " << sVector << " " << sResult.first << "/" << sResult.second << "\n";
-	cout << "Longest chain: " << lVector << " " << lResult.sum << "/" << lResult.count << "\n";
 
 	return 0;
 }
