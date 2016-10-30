a852b5521bc8d41fc8879bce47b28b280f7fcc1a
Martin Babka <babky@users.noreply.github.com>
2016-10-30 21:30:33 +0100

    Fixing the generation of Eclipse project.

--- src/hash/cmake/cmake_eclipse.cmd
+++ src/hash/cmake/cmake_eclipse.cmd
@@ -1,1 +1,5 @@
 cmake -G "Eclipse CDT4 - MinGW Makefiles" -D "BOOST_ROOT=D:\source\boost_1_59_0" . 
+
+rem For MSYS2 do the following
+rem Add MSYS2\usr\bin and MSYS2\mingw64\bin to PATH (install mingw, boost, make via pacman)
+rem Generate project using CDT4 Unix Makefiles
