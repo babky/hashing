6849409b0c3b067af9af06f7e3681d64371b7886
Martin Babka <babky@users.noreply.github.com>
2016-11-28 16:47:12 +0100

    Better documenting the way how to set up the project.

--- src/hash/cmake/cmake_eclipse.cmd
+++ src/hash/cmake/cmake_eclipse.cmd
@@ -1,5 +1,10 @@
-cmake -G "Eclipse CDT4 - MinGW Makefiles" -D "BOOST_ROOT=D:\source\boost_1_59_0" . 
+rem cmake -G "Eclipse CDT4 - MinGW Makefiles" -D "BOOST_ROOT=D:\source\boost_1_59_0" . 
 
 rem For MSYS2 do the following
 rem Add MSYS2\usr\bin and MSYS2\mingw64\bin to PATH (install mingw, boost, make via pacman)
 rem Generate project using CDT4 Unix Makefiles
+rem Set the proper BOOST_ROOT (download boost and use a local copy)
+rem Use local cmake, i.e. download and unpacke windows cmake
+rem the Final command may look like the following line
+
+C:\Programs\cmake-3.7.0-rc1-win64-x64\bin\cmake -G "Eclipse CDT4 - Unix Makefiles" -D "BOOST_ROOT=C:\Users\babky\Documents\source\boost_1_62_0" . 
