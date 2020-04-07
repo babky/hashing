88a61645fb90c8d79cc493642b8caa87a5ed40b7
Martin Babka <babkys@gmail.com>
2020-04-07 07:43:33 +0200

    Fix build, remove VC support

---
+++ src/hash/cmake/bootstrap.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+DIR=`dirname "$0"`
+DIR=`realpath "${DIR}"`
+CMAKE=${CMAKE:-cmake}
+
+cd "${DIR}/.."
+if [ ! -f vcpkg/vcpkg ]
+then
+  git clone https://github.com/Microsoft/vcpkg.git
+  cd vcpkg
+  ./bootstrap-vcpkg.sh
+else
+  cd vcpkg
+fi
+
+# Quality enforcement
+./vcpkg install gtest
+./vcpkg install glog
+
+# Libraries
+./vcpkg install boost-date-time
+./vcpkg install boost-log
+./vcpkg install boost-program-options
+./vcpkg install boost-random
+./vcpkg install boost-serialization
+./vcpkg install boost-system
+./vcpkg install boost-thread
+./vcpkg install boost-ublas
+
+cd "${DIR}/.."
+if [ ! -d "paradiseo" ]
+then
+  # git clone git://scm.gforge.inria.fr/paradiseo/paradiseo.git 
+  git clone git@github.com:babky/paradiseo.git
+  cd paradiseo
+  mkdir build
+  $CMAKE .. -DINSTALL_TYPE=full
+  make
+fi 
+
+cd "${DIR}"
+$CMAKE -DCMAKE_MODULE_PATH="${DIR}/../paradiseo/cmake/module" -DPARADISEO_ROOT="${DIR}/../paradiseo" -DCMAKE_TOOLCHAIN_FILE="`pwd`/../vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_BUILD_TYPE=Debug -DBOOST_ROOT='${DIR}/../vcpkg/installed/x64-linux/' .
